27e3d43a26bfb7b39bc64df54ec19278639574ca
gug007 <gug007@i.ua>
2016-08-31 12:11:23 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+curl -k -v https://localhost:3005
+
+curl -k -v --cert cert/user.p12:123456 https://localhost:3005
---
+++ server.js
@@ -0,0 +1,32 @@
+const https = require('https')
+const fs = require('fs')
+
+const PORT  = 3005
+const SUCCESS = 'SUCCESS'
+const FAILURE = 'FAILURE'
+const CERT_PATH = 'cert/'
+
+const getFile = filename => fs.readFileSync(CERT_PATH + filename)
+
+const send = (res, message) => {
+  res.end(message);
+  console.log(message);
+}
+
+const options = {
+  key: getFile('server.key'),
+  cert: getFile('server.crt'),
+  ca: getFile('ca.crt'),
+  passphrase: 'qwerty',
+  requestCert: true,
+  rejectUnauthorized: false
+};
+
+const server = https.createServer(options, (req, res) => {
+  console.log(req.client.authorized)
+  req.client.authorized
+    ? send(res, SUCCESS)
+    : send(res, FAILURE)
+})
+
+server.listen(PORT, () => console.log(`listen port: ${PORT}`));
